088b70755d21f1d9bc753f3edeeade6d188f6cce
thorium <157016164+nikilokk@users.noreply.github.com>
2026-02-24 00:40:17 +0100

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -3,74 +3,90 @@
 using namespace std;
 
 int main() {
-    float a,b;
-    float result;
-    char symbol;
-    bool operation_valid=true;
-
     while(true) {
-    cout<<"enter the first number or type 'q' to quit: "<<endl;
+
+    bool operation_valid=true;
+    string op;
+    char symbol;
+    float b, a, result;
+
+    cout<<"do you want to continue? Y/N"<<endl;
+    cin>>op;
+
+    if(op=="N") {
+        return 0;
+    } 
+    else if (op!="Y")
+    {
+        cout<<"wrong option!"<<endl;
+        operation_valid = false;
+        continue;
+    }
+
+    cout<<"enter the first number: "<<endl;
     cin>>a;
     cout<<"enter the second number: "<<endl;
     cin>>b;
 
     cout<<"choose a mathematical operation: "<<endl;
-    cout<<"*,/,+,-,%";
+    cout<<"*,/,+,-,%"<<endl;
     cin>>symbol;
+
+    if(symbol=='%' && a!=(int)a || b!=(int)b) {
+        cout<<"numbers has fractional part"<<endl;
+        continue;
+    }
 
 
     int ai, bi;
 
-    switch (symbol)
-    {
-    case '*':
-        result = a*b;
-        break;
-    case '/': {
-        ai = (int)a;
-        bi = (int)b;
+    // mathematical operations 
+        switch (symbol)
+        {
+        case '*':
+            result = a*b;
+            break;
+        case '/': {
 
-        if(bi==0) {
-            cout<<"you can't divide by 0"<<endl;
-            operation_valid = false;
+            if(b==0) {
+                cout<<"you can't divide by 0"<<endl;
+                operation_valid = false;
+                break;
+            }
+            result = a/b;
             break;
         }
-        result = ai/bi;
-        break;
-    } 
-    break;
 
 
-    case '-':
-        result = a-b;
-        break;
-    case '+':
-        result = a+b;
-        break;
-    case '%': {
-        ai = (int)a;
-        bi = (int)b;
+        case '-':
+            result = a-b;
+            break;
+        case '+':
+            result = a+b;
+            break;
+        case '%': {
+            ai = (int)a;
+            bi = (int)b;
+            if(bi==0) {
+                cout<<"you can't divide by 0"<<endl;
+                operation_valid = false;
+                break;
+            }
+            
 
-        if(bi==0) {
-            cout<<"you can't divide by 0"<<endl;
-            operation_valid = false;
+            result = ai%bi;
             break;
         }
-        
-
-        result = ai%bi;
         break;
-    }
-    break;
+            
         
+        default:
+            operation_valid = false;
+            cout<<"you have to choose one of the operators!"<<endl;
+            break;
+        }
     
-    default:
-        operation_valid = false;
-        cout<<"you have to choose one of the operators!"<<endl;
-        break;
-    }
-    
-
+    // prints result if operation_valid equals true
     if(operation_valid) {
     cout<<"the result is: "<<result<<endl;
     continue;
